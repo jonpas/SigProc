56cd88cc76bdc7088ab69c44c425a9179409658f
jonpas <jonpas33@gmail.com>
2018-03-13 21:33:37 +0100

    Add ability to pause and resume audio playback

--- sigproc.py
+++ sigproc.py
@@ -13,7 +13,7 @@ from pydub import AudioSegment, exceptions
 from pydub.utils import make_chunks
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, QMutex, QWaitCondition
 from PyQt5.QtWidgets import *
 
 PyAudio = pyaudio.PyAudio
@@ -21,6 +21,7 @@ PyAudio = pyaudio.PyAudio
 
 class MainWindow(QWidget):
     sig_sound_stop = pyqtSignal()
+    sig_sound_pause = pyqtSignal()
 
     def __init__(self):
         super().__init__()
@@ -34,6 +35,7 @@ class MainWindow(QWidget):
         self.line_over = None
 
         self.sound_thread = None
+        self.sound_paused = False
 
         self.initUI()
 
@@ -50,6 +52,9 @@ class MainWindow(QWidget):
         # Audio controls
         self.btn_pause = QPushButton("Pause")
         self.btn_pause.setDisabled(True)
+        self.btn_pause.clicked.connect(self.sound_pause)
+        self.sound_mutex = QMutex()
+        self.sound_pause_cond = QWaitCondition()
 
         self.btn_play = QPushButton("Play")
         self.btn_play.setDisabled(True)
@@ -87,6 +92,7 @@ class MainWindow(QWidget):
 
     def update_ui(self, block):
         self.btn_pause.setDisabled(not block)
+        self.btn_pause.setText("Resume" if self.sound_paused else "Pause")
         self.btn_play.setDisabled(block)
         self.btn_stop.setDisabled(not block)
 
@@ -101,6 +107,8 @@ class MainWindow(QWidget):
             self.txt_file.setText("")
 
     def load_signal(self, file):
+        self.sound_stop()
+
         try:
             self.sound = AudioSegment.from_file(file)
             signal = self.sound.get_array_of_samples()
@@ -174,18 +182,30 @@ class MainWindow(QWidget):
         self.figure.canvas.blit(ax.bbox)
 
     def sound_play(self):
-        if self.sound_thread:
-            print("sound thread already running!")
-        else:
-            if self.is_signal_loaded():
-                self.sound_thread = SoundThread(self.sound)
-                self.sound_thread.finished.connect(self.on_sound_done)
-                self.sig_sound_stop.connect(self.sound_thread.stop)
-                self.sound_thread.start()
-                self.update_ui(True)
+        if not self.sound_thread and self.is_signal_loaded():
+            self.sound_thread = SoundThread(self.sound, self.sound_mutex, self.sound_pause_cond)
+            self.sound_thread.finished.connect(self.on_sound_done)
+            self.sig_sound_stop.connect(self.sound_thread.stop)
+            self.sig_sound_pause.connect(self.sound_thread.pause)
+            self.sound_thread.start()
+            self.update_ui(True)
 
     def sound_stop(self):
         self.sig_sound_stop.emit()
+        self.sound_mutex.lock()
+        self.sound_pause_cond.wakeAll()
+        self.sound_mutex.unlock()
+
+    def sound_pause(self):  # Toggle
+        if self.sound_paused:
+            self.sig_sound_pause.emit()
+            self.sound_mutex.lock()
+            self.sound_pause_cond.wakeAll()
+            self.sound_mutex.unlock()
+        else:
+            self.sig_sound_pause.emit()
+        self.sound_paused = not self.sound_paused
+        self.update_ui(True)
 
     def on_sound_done(self):
         self.sound_thread = None
@@ -193,11 +213,15 @@ class MainWindow(QWidget):
 
 
 class SoundThread(QThread):
-    def __init__(self, sound):
+    def __init__(self, sound, mutex, pause_cond):
         QThread.__init__(self)
 
         self.sound = sound
+
         self.running = True
+        self.paused = False
+        self.mutex = mutex
+        self.pause_cond = pause_cond
 
     def __del__(self):
         self.wait()
@@ -210,16 +234,23 @@ class SoundThread(QThread):
             rate=self.sound.frame_rate,
             output=True)
 
-        # Break into half-second chunks
-        for chunk in make_chunks(self.sound, 500):
-            if self.running:
-                stream.write(chunk._data)
-            else:
+        # Break into 0.1 second chunks
+        for chunk in make_chunks(self.sound, 100):
+            if not self.running:
                 break
 
-        stream.stop_stream()
+            stream.write(chunk._data)
+
+            if self.paused:
+                self.mutex.lock()
+                self.pause_cond.wait(self.mutex)
+                self.mutex.unlock()
+
         stream.close()
         p.terminate()
+
+    def pause(self):  # Toggle
+        self.paused = not self.paused
 
     def stop(self):
         self.running = False
