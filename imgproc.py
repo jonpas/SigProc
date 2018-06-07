75301ba51c21d47dfca1a0b31385ca088b94bec8
jonpas <jonpas33@gmail.com>
2018-06-07 11:25:56 +0200

    Switch to OpenCV for Image Processor

--- imgproc.py
+++ imgproc.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+import cv2
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
@@ -27,9 +32,17 @@ class MainWindow(QWidget):
         self.btn_save.setDisabled(True)
         self.btn_save.clicked.connect(self.show_save_dialog)
 
-        # Image space
-        self.canvas = QLabel()
-        self.canvas.setStyleSheet("QLabel { background-color: white; }")
+        # Graph space
+        self.figure = Figure()
+        FigureCanvas(self.figure)
+        self.figure.canvas.setMinimumHeight(400)
+        self.subplot = self.figure.add_subplot(1, 1, 1)
+        self.subplot.axis("off")
+
+        # Graph toolbar
+        self.plotnav = NavigationToolbar(self.figure.canvas, self.figure.canvas)
+        self.plotnav.setStyleSheet("QToolBar { border: 0px }")
+        self.plotnav.setOrientation(Qt.Vertical)
 
         # Layout
         hbox_top = QHBoxLayout()
@@ -41,7 +54,7 @@ class MainWindow(QWidget):
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox_top)
-        vbox.addWidget(self.canvas)
+        vbox.addWidget(self.figure.canvas)
 
         # Window
         self.setLayout(vbox)
@@ -49,37 +62,44 @@ class MainWindow(QWidget):
         self.setWindowTitle("Signal Processor - Image")
         self.show()
 
+    # Overriden resize event
+    def resizeEvent(self, resizeEvent):
+        self.plotnav.move(self.width() - 55, 0)
+
     def update_ui(self):
         self.btn_save.setDisabled(not self.is_image_loaded())
 
     def show_open_dialog(self):
-        fname = QFileDialog.getOpenFileName(self, "Open file", filter="Image (*.png *.jpg *.bmp)")
-        if fname[0] and self.load_image(fname[0]):
-            self.txt_file.setText(fname[0])
+        fname, ext = QFileDialog.getOpenFileName(self, "Open file", filter="Image (*.png *.jpg *.bmp)")
+        if fname and self.load_image(fname):
+            self.txt_file.setText(fname)
 
     def show_save_dialog(self):
-        fname = QFileDialog.getSaveFileName(self, "Save file", filter="Image (*.png *.jpg *.bmp)")
-        if fname[0] and self.is_image_loaded():
-            try:
-                self.img.save(fname[0])
-            except exceptions.CouldntEncodeError:
-                print("Failed to save image!")
-            else:
-                self.txt_file.setText(fname[0])
+        fname, ext = QFileDialog.getSaveFileName(self, "Save file", filter="Image (*.png *.jpg *.bmp)")
+        if fname and self.is_image_loaded():
+            # Save as PNG if not set
+            if '.' not in fname:
+                fname += ".png"
+
+            cv2.imwrite(fname, self.img)
+            self.txt_file.setText(fname)
 
     def load_image(self, file):
-        try:
-            self.img = QPixmap(file)
-            self.canvas.setPixmap(self.img.scaled(self.canvas.width(), self.canvas.height(), Qt.KeepAspectRatio))
-        except exceptions.CouldntDecodeError:
-            print("Failed to load image!")
+        if not os.path.isfile(file):
             return False
-        else:
-            self.update_ui()
-            return True
+
+        self.img = cv2.imread(file)
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.plot(self.img)
+        self.update_ui()
+        return True
 
     def is_image_loaded(self):
         return self.img is not None
+
+    def plot(self, img):
+        self.subplot.imshow(img)
+        self.figure.canvas.draw()
 
 
 if __name__ == "__main__":
